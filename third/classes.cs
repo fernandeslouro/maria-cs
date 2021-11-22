206bb14777e632ac22c276e58da8476e6035e9b4
fernandeslouro <jd.louro@outlook.pt>
2021-11-22 23:03:11 +0000

    implement simplified input function

--- third/classes.cs
+++ third/classes.cs
@@ -73,45 +73,166 @@ namespace third
 
 
 
-    public Client inputClient(){
+    public Client inputClient()
+    {
     }
 
-    public Client inputClient(){
+    public Client inputClient()
+    {
         // Id, Firstname, MiddleName, Lastname, DOB, CaseType, Street, Steet Nr, Zip, City
         Client input_client = new Client;
 
-        Console.WriteLine("Input Id");
-        Client.Id = Console.ReadLine();
+        bool correct = false;
+        string correct_string;
 
-        Console.WriteLine("Input FirstName");
-        Client.FirstName = Console.ReadLine();
+        while (!correct)
+        {
+            Console.WriteLine("Input Id");
+            input_client.Id = Console.ReadLine();
 
-        Console.WriteLine("Input Middlename");
-        Client.Middlename = Console.ReadLine();
+            Console.WriteLine("Input FirstName");
+            input_client.FirstName = Console.ReadLine();
 
-        Console.WriteLine("Input Lastname");
-        Client.Lastname = Console.ReadLine();
+            Console.WriteLine("Input Middlename");
+            input_client.Middlename = Console.ReadLine();
 
-        Console.WriteLine("Input DOB");
-        Client.DOB = Console.ReadLine();
+            Console.WriteLine("Input Lastname");
+            input_client.Lastname = Console.ReadLine();
 
-        Console.WriteLine("Input CaseType");
-        Client.CaseType = Console.ReadLine();
+            Console.WriteLine("Input DOB");
+            input_client.DOB = Console.ReadLine();
 
-        Console.WriteLine("Input Street");
-        Client.Street = Console.ReadLine();
+            Console.WriteLine("Input CaseType");
+            input_client.CaseType = Console.ReadLine();
 
-        Console.WriteLine("Input Street Nr");
-        Client.Street_Nr = Console.ReadLine();
+            Console.WriteLine("Input Street");
+            input_client.Street = Console.ReadLine();
 
-        Console.WriteLine("Input Zip");
-        Client.Zip = Console.ReadLine();
+            Console.WriteLine("Input Street Nr");
+            input_client.Street_Nr = Console.ReadLine();
 
-        Console.WriteLine("Input City");
-        Client.City = Console.ReadLine();
+            Console.WriteLine("Input Zip");
+            input_client.Zip = Console.ReadLine();
 
+            Console.WriteLine("Input City");
+            input_client.City = Console.ReadLine();
 
+            Console.WriteLine("The input Client is:\n");
+            Console.WriteLine($"Id\t Firstname\t MiddleName\t Lastname\t DOB\t CaseType\t Street\t Steet_Nr\t Zip\t City\t ");
+            Console.WriteLine($"{input_client.Id}\t {input_client.Firstname}\t {input_client.MiddleName}\t {input_client.Lastname}\t {input_client.DOB}\t {input_client.CaseType}\t {input_client.Street}\t {input_client.Steet_Nr}\t {input_client.Zip}\t {input_client.City}\t ");
+
+            Console.WriteLine("\nIs it Correct? (y/N)");
+            correct_string = Console.ReadLine();
+            if (correct_string == "y")
+            {
+                correct = true;
+            }
+        }
+        return input_client;
     }
+
+
+    public Client inputAppointment()
+    {
+        //Id, ClientId, LawyerId, Datetime, MeetingRoom, ShortDescription
+        Appointment input_appointment = new Appointment;
+
+        bool correct = false;
+        string correct_string;
+
+        while (!correct)
+        {
+            Console.WriteLine("Input Id");
+            input_appointment.Id = Console.ReadLine();
+
+            Console.WriteLine("Input ClientId");
+            input_appointment.ClientId = Console.ReadLine();
+
+            Console.WriteLine("Input LawyerId");
+            input_appointment.LawyerId = Console.ReadLine();
+
+            Console.WriteLine("Input Datetime");
+            input_appointment.Datetime = Console.ReadLine();
+
+            Console.WriteLine("Input MeetingRoom");
+            input_appointment.MeetingRoom = Console.ReadLine();
+
+            Console.WriteLine("Input ShortDescription");
+            input_appointment.CaseType = Console.ReadLine();
+
+            Console.WriteLine("The input Appointment is:\n");
+
+            Console.WriteLine($"Id\t ClientId\t LawyerId\t Datetime\t MeetingRoom\t ShortDescription");
+            Console.WriteLine($"{input_appointment.ClientId}\t {input_appointment.LawyerId}\t {input_appointment.Datetime}\t {input_appointment.MeetingRoom}\t {input_appointment.ShortDescription}");
+
+            Console.WriteLine("\nIs it Correct? (y/N)");
+            correct_string = Console.ReadLine();
+            if (correct_string == "y")
+            {
+                correct = true;
+            }
+        }
+        return input_appointment;
+    }
+
+    public dynamic inputInfo(string class_type)
+    {
+        if (class_type == "Client")
+        {
+            Client input = new Client();
+            PropertyInfo[] properties = typeof(Client).GetProperties();
+        }
+        else if (class_type == "Appointment")
+        {
+            Appointment input = new Case();
+            PropertyInfo[] properties = typeof(Appointment).GetProperties();
+        }
+        else if (class_type == "Case")
+        {
+            Case input = new Case();
+            PropertyInfo[] properties = typeof(Case).GetProperties();
+        }
+
+        string value;
+        string correct = false;
+
+        while (!correct)
+        {
+            foreach (PropertyInfo p in properties)
+            {
+                Console.WriteLine($"Input property");
+                value = Console.ReadLine;
+                case_property.SetValue(input, value);
+            }
+
+
+            Console.WriteLine("The input Appointment is:\n");
+
+            foreach (var p in properties)
+            {
+                Console.Write(p.Name);
+                Console.Write("\t");
+            }
+            Console.WriteLine("");
+            foreach (var p in properties)
+            {
+                Console.Write(p.GetValue(input));
+                Console.Write("\t");
+            }
+
+
+            Console.WriteLine("\nIs it Correct? (y/N)");
+            correct_string = Console.ReadLine();
+            if (correct_string == "y")
+            {
+                correct = true;
+            }
+
+        }
+
+        return input;
+    }
+
 
     public List<Lawyer> ImportFile(string path_layers_file)
     {
